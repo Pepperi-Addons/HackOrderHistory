d70c0a62f87d64fded465e86db1f09c912fddcf2
Nofar Elarat <nofar.e@pepperi.com>
2022-03-23 18:06:13 +0200

    add get to kibana

--- server-side/api.ts
+++ server-side/api.ts
@@ -2,15 +2,20 @@ import MyService from './my.service'
 import { Client, Request } from '@pepperi-addons/debug-server'
 import { PapiClient } from '@pepperi-addons/papi-sdk'
 
-export async function get_sql_data(client: Client, request: Request){
-    const service = new MyService(client)
-    const res = await service.GetSQLData(request.query["order_id"]);
-    return res;
-}
-
 export async function get_header_data_from_sql(client: Client, request: Request){
     const service = new MyService(client)
     const res = await service.GetHeaderDataFromSQL(request.query["order_id"]);
     return res;
 }
 
+export async function get_sql_data(client: Client, request: Request){
+    const service = new MyService(client)
+    const res = await service.GetSQLData(request.query["order_id"]);
+    return res;
+}
+
+export async function get_kibana_data(client: Client, request: Request){
+    const service = new MyService(client)
+    const res = await service.GetKibanaData(request.query["order_uuid"].toLowerCase());
+    return res;
+}
